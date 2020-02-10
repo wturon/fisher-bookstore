14995af8a1a37e1f5088291340e8767f131ce808
wturon <59670834+wturon@users.noreply.github.com>
2020-02-10 17:17:29 -0500

    Adding the home shell (#17)

---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return(
+        <div className="Home">
+            <div className="lander">
+                <h1>Home</h1>
+                <p1>Welcome to Fisher Bookstore.</p1>
+            </div>
+        </div>
+    );
+}
