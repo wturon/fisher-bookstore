5bebc1ccbc837b9a12b431d5d9ff954ab2560e3c
William Turon <wturon1@gmail.com>
2020-02-10 17:46:22 -0500

    Adding books feature

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
+import Books from "./Books/Books";
 
 export default function Routes(){
     return(
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/Books" exact component={Books} />
         </Switch>
     );
 }
