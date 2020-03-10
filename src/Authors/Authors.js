e7685af60bc1b60b98b4c6ad7994b7e3a01b9677
wturon <59670834+wturon@users.noreply.github.com>
2020-03-10 13:32:40 -0400

    Integrating fisher-bookstore and bookstore api (#24)

--- src/Authors/Authors.js
+++ src/Authors/Authors.js
@@ -1,46 +1,22 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./Authors.css";
 import AuthorDisplay from "./AuthorDisplay";
 
-export default class Authors extends Component {
-   constructor(props){
-       super(props);
-       this.state = {
-           authors: [
-               {
-                   id: 1,
-                   name: "James",
-               },
-               {
-                   id: 2,
-                   name: "Mark",
-                },
-                {
-                   id: 3,
-                   name: "Eric Evans",
-                },
-                {
-                   id: 4,
-                   name: "William Turon",
-                },
-                {
-                    id: 5,
-                    name: "Noah Graifer",
-                }
-           ]
-       };
-   }
+export default function Authors(props) {
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+       fetch("https://localhost:5001/api/authors/")
+       .then(response => response.json())
+       .then(data => setData(data));
+   },[])
+
    
-   
-   
-   
-   render(){
     return(
         <div className="Author">
             <div className="lander">
-                <AuthorDisplay author={this.state.authors} />
+                <AuthorDisplay author={data} />
             </div>
         </div>
     );
-  }
-}
+};
--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,73 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./Books.css";
 import BookDisplay from "./BookDisplay";
 
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217",
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331",
-                },
-                {
-                    id: 3,
-                    title: "The Big Bang",
-                    author: "Nicole Frank",
-                    isbn: "978-1942788332",
-                },
-                {
-                    id: 4,
-                    title: "Willy Wonka",
-                    author: "Willy  Forsgren",
-                    isbn: "978-1942788333",
-                },
-                {
-                    id: 5,
-                    title: "The Bible",
-                    author: "Nicole Anderson",
-                    isbn: "978-1942788334",
-                },
-                {
-                    id: 6,
-                    title: "The Book of James",
-                    author: "Mark",
-                    isbn: "978-1942788335",
-                },
-                {
-                    id: 7,
-                    title: "The Book of Mark",
-                    author: "James",
-                    isbn: "978-1942788336",
-                },
-                {
-                    id: 8,
-                    title: "Book",
-                    author: "Book man",
-                    isbn: "978-1942788337",
-                }
-            ]
-        };
-    }
+export default function Books(props) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
   
-  render(){
     return(
         <div className="Books">
             <div className="lander">
-                <BookDisplay books={this.state.books} />
+                <BookDisplay books={data} />
             </div>
         </div>
     );
-  }
-    
 }
